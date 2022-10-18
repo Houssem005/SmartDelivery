fcb2a160595b1b2e584b9c5324ec5a73e37db4e4
Chebbihoussemeddine <79277235+Chebbihoussemeddine@users.noreply.github.com>
2022-10-19 00:44:46 +0100

    Smart delivery

---
+++ smart delivery backend/src/main/java/com/esprit/microservice/SmartDeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDeliveryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDeliveryBackendApplication.class, args);
+	}
+
+}
